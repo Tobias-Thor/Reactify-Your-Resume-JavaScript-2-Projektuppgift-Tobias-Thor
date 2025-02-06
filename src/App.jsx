7fe74779afaba85b48f6739beb405654b033550a
Tobias Thor <tobiasthor@protonmail.com>
2025-02-06 22:00:55 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,58 @@ import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import "./App.css";
 
-// Functional component that serves as the main structure for the app
 function App() {
+  // Data to pass as props to Home component
+  const homeData = {
+    title: "Välkommen till min Portfolio",
+    intro: "Jag är en blivande junior front-end-utvecklare med erfarenhet av:",
+    skills: [
+      "HTML (semantisk)",
+      "WACG",
+      "SEO",
+      "CSS",
+      "SASS/SCSS",
+      "Tailwind",
+      "JavaScript",
+      "React",
+      "Redux",
+      "SPA",
+      "Node.js",
+      "bash",
+      "VS Code",
+      "GitHub",
+      "Trello",
+      "Figma"
+    ]
+  };
+
   return (
     <>
       <Provider store={store}>
-      <Header /> {/* The Header component that appears at the top of the page */}
-      <main>
-        {/* Routes component to define different routes in the app */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </main>
-      <Footer /> {/* The Footer component that appears at the bottom of the page */}
+        <Header /> {/* The Header component that appears at the top of the page */}
+        <main>
+          {/* Routes component to define different routes in the app */}
+          <Routes>
+            <Route 
+              path="/" 
+              element={<Home 
+                title={homeData.title} 
+                intro={homeData.intro} 
+                skills={homeData.skills} 
+              />} 
+            />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer /> {/* The Footer component that appears at the bottom of the page */}
       </Provider>
     </>
   );
 }
+
+export default App;
+
 
 export default App; // Exporting the App component to be used in other parts of the app
